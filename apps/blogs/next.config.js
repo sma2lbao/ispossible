d6813642ed6d95cd55a31358760290ffc8a912af
sma2lbao <sma2lbao@qq.com>
2024-02-29 16:49:08 +0800

    test: 追踪mdx文件

--- apps/blogs/next.config.js
+++ apps/blogs/next.config.js
@@ -3,16 +3,24 @@ const stylexPlugin = require('@stylexjs/nextjs-plugin');
 const path = require('path');
 
 /** @type {import('next').NextConfig} */
+const config = {
+  transpilePackages: ['@stylexjs/open-props'],
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+   // Optionally, add any other Next.js config below
+  //  output: 'standalone',
+   experimental: {
+    outputFileTracingIncludes: {
+      '/articles': ['./mdxs/**/*']
+    }
+   }
+}
+
 module.exports = stylexPlugin({
   aliases: {
     '@/*': [path.join(__dirname, '*')],
   },
   rootDir: __dirname,
-})({
-  transpilePackages: ['@stylexjs/open-props'],
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-   // Optionally, add any other Next.js config below
-});
+})(config);
 
 
