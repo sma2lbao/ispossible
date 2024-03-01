95774b5dc3a8cf54db5899c3fc84b92fb07f643f
sma2lbao <sma2lbao@qq.com>
2024-03-01 14:25:05 +0800

    chore: 样式库stylex bug问题在引入css文件时不引入生成的css文件

--- apps/blogs/app/_layouts/document.tsx
+++ apps/blogs/app/_layouts/document.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Footer, Topbar } from "@design/pro";
 import stylex from "@stylexjs/stylex";
 import { useTheme, Theme } from "@design/core";
-import "normalize.css";
 import Logo from "./logo";
 
 export interface DocumentProps {
