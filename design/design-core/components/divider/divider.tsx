6b7a23c8327295c4b2ea5e4a23b123c71274e9df
sma2lbao <sma2lbao@qq.com>
2024-03-07 15:24:09 +0800

    ci: 更新 design core 流水线

---
+++ design/design-core/components/divider/divider.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import stylex, { type StyleXStyles } from "@stylexjs/stylex";
+
+export interface DividerProps {
+  /**
+   * 水平还是垂直类型
+   * @default "x"
+   */
+  direction: "x" | "y";
+
+  /**
+   * 分割线样式对象
+   */
+  style: StyleXStyles;
+
+  children?: React.ReactNode;
+}
+
+const styles = stylex.create({
+  root: {
+    display: "flex",
+    clear: "both",
+    width: "100%",
+    minWidth: "100%",
+    margin: "0.5em 0",
+  },
+  vertical: {},
+});
+
+export const Divider: React.FC<DividerProps> = (props) => {
+  const { direction = "x", children } = props;
+
+  return <div {...stylex.props(styles.root)}>{children}</div>;
+};
