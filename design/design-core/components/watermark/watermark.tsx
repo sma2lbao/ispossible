b7b3dc2f9c11b4a68ee5a72ea7cecd0fb79d782c
sma2lbao <sma2lbao@qq.com>
2024-03-29 17:31:45 +0800

    feat(wip): 水印组件设置

--- design/design-core/components/watermark/watermark.tsx
+++ design/design-core/components/watermark/watermark.tsx
@@ -1,4 +1,5 @@
 import React, { useLayoutEffect, useRef } from "react";
+import stylex from "@stylexjs/stylex";
 
 interface WatermarkProps {
   gap?: [number, number];
@@ -19,41 +20,18 @@ export const Watermark: React.FC<WatermarkProps> = (props) => {
   const { children, image, content } = props;
   const rootRef = useRef<HTMLDivElement>(null);
 
-  const renderWatermark = () => {
+  const createWatermark = () => {
     const canvas = document.createElement("canvas");
-    const context = canvas.getContext("2d")!;
-    if (image) {
-      const resource = new Image();
-      resource.onload = () => {
-        drawWatermark(resource);
-      };
-      resource.onerror = () => {
-        drawWatermark(content);
-      };
-      resource.src = image;
-      resource.crossOrigin = "anonymous";
-      resource.referrerPolicy = "no-referrer";
-    } else {
-      drawWatermark(content);
-    }
+    const context = canvas.getContext("2d");
+    if (!context) return;
 
-    function drawWatermark(origin?: string | HTMLImageElement) {
-      if (!origin) return;
-      if (origin instanceof HTMLImageElement) {
-        context.drawImage(origin, 0, 0, origin.width, origin.height);
-      } else {
-        context.font = "14px serif";
-        context.fillText(origin, 10, 50);
-      }
-      const base64 = canvas.toDataURL();
-      if (rootRef.current) {
-        rootRef.current.style.backgroundImage = `url("${base64}")`;
-      }
-    }
+    const createText = (
+      font: Pick<React.CSSProperties, "fontFamily" | "fontSize" | "fontWeight">
+    ) => {};
   };
 
   useLayoutEffect(() => {
-    renderWatermark();
+    createWatermark();
   });
 
   return <div ref={rootRef}>{children}</div>;
--- design/design-core/docs/stories/watermark.stories.tsx
+++ design/design-core/docs/stories/watermark.stories.tsx
@@ -18,14 +18,32 @@ type Story = StoryObj<typeof meta>;
 
 export const 代码演示: Story = {
   args: {
-    children: <div style={{ height: 200 }}>Primary</div>,
-    content: "水印",
+    children: (
+      <div style={{ height: 300, width: 400, border: "1px solid #ddd" }}>
+        怒髮衝冠，憑闌處，瀟瀟雨歇。
+        <br />
+        抬望眼，仰天長嘯，壯懷激烈。
+        <br />
+        三十功名塵與土，八千里路雲和月。
+        <br />
+        莫等閒，白了少年頭，空悲切。
+        <br />
+        靖康恥，猶未雪； 臣子恨，何時滅。
+        <br />
+        駕長車踏破賀蘭山缺。
+        <br />
+        壯志饑餐胡虜肉，笑談渴飲匈奴血。
+        <br />
+        待從頭，收拾舊山河，朝天闕。
+      </div>
+    ),
+    content: "满江红",
   },
 };
 
-export const 简单使用: Story = {
-  args: {
-    children: <div style={{ height: 200 }}>测试内容区域</div>,
-    content: "水印",
-  },
-};
+// export const 简单使用: Story = {
+//   args: {
+//     children: <div style={{ height: 200 }}>测试内容区域</div>,
+//     content: "水印",
+//   },
+// };
