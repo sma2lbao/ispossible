b1ad76b366caafd661395655c4446c4d576a5582
sma2lbao <sma2lbao@qq.com>
2024-05-07 16:21:22 +0800

    feat(design-core): 增加分页组件

---
+++ design/design-core/components/pagination/pagination.tsx
@@ -0,0 +1,128 @@
+import React, { useMemo } from "react";
+import stylex from "@stylexjs/stylex";
+import { Space } from "../space";
+import { Button } from "../button";
+import "@design/icon/right";
+import "@design/icon/left";
+
+const styles = stylex.create({
+  root: {},
+  ellipsis: {
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    verticalAlign: "super",
+  },
+});
+
+export interface PaginationProps {
+  /**
+   * 总条目数
+   */
+  total?: number;
+  /**
+   * 每页条目数
+   * @default 20
+   */
+  pageSize?: number;
+  /**
+   * 当前页
+   * @default 1
+   */
+  page?: number;
+  onPageChange?: (nextPage: number) => void;
+}
+
+const MAX_DISPLAY = 5;
+
+export const Pagination: React.FC<PaginationProps> = (props) => {
+  const { total = 0, pageSize = 10, page = 1, onPageChange } = props;
+  const maxPage = Math.ceil(total / pageSize);
+  const visiblePages = useMemo<number[]>(() => {
+    if (maxPage <= MAX_DISPLAY) {
+      return Array(maxPage)
+        .fill(undefined)
+        .map((_, index) => index + 1);
+    }
+    if (page === 1 || page - 1 === 1) {
+      return Array(MAX_DISPLAY)
+        .fill(undefined)
+        .map((_, index) => 1 + index);
+    }
+    if (page + 1 === maxPage || page === maxPage) {
+      return Array(MAX_DISPLAY)
+        .fill(undefined)
+        .map((_, index) => maxPage - index)
+        .reverse();
+    }
+    return [page - 2, page - 1, page, page + 1, page + 2];
+  }, [page, maxPage]);
+
+  const handleClickNumber = (nextPage: number) => {
+    if (nextPage !== page) {
+      onPageChange?.(nextPage);
+    }
+  };
+
+  const handleClickPervious = () => {
+    onPageChange?.(page - 1);
+  };
+
+  const handleClickNext = () => {
+    onPageChange?.(page + 1);
+  };
+
+  return (
+    <div {...stylex.props(styles.root)}>
+      <Space>
+        <Button disabled={page === 1} onClick={handleClickPervious} type="text">
+          <is-left />
+        </Button>
+
+        {!visiblePages.includes(1) && (
+          <Button
+            type={page === 1 ? "primary" : "text"}
+            onClick={() => handleClickNumber(1)}
+          >
+            1
+          </Button>
+        )}
+
+        {visiblePages[0] > 2 && (
+          <span {...stylex.props(styles.ellipsis)}>...</span>
+        )}
+
+        {visiblePages.map((item, index) => (
+          <Button
+            key={"page-" + item + ":" + index}
+            type={page === item ? "primary" : "text"}
+            onClick={() => handleClickNumber(item)}
+          >
+            {item}
+          </Button>
+        ))}
+
+        {visiblePages[MAX_DISPLAY - 1] < maxPage - 1 && (
+          <span {...stylex.props(styles.ellipsis)}>...</span>
+        )}
+
+        {!visiblePages.includes(maxPage) && (
+          <Button
+            type={page === maxPage ? "primary" : "text"}
+            onClick={() => handleClickNumber(maxPage)}
+          >
+            {maxPage}
+          </Button>
+        )}
+
+        <Button
+          disabled={maxPage === page}
+          onClick={handleClickNext}
+          type="text"
+        >
+          <is-right />
+        </Button>
+      </Space>
+    </div>
+  );
+};
