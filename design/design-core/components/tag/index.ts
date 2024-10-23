457b9a6fdea02a880e12e09612653463a9c8fce7
sma2lbao <sma2lbao@qq.com>
2024-10-23 16:35:04 +0800

    feat: 优化 Tag 组件

--- design/design-core/components/tag/index.ts
+++ design/design-core/components/tag/index.ts
@@ -1,1 +1,3 @@
 export * from "./tag";
+
+export * from "./group";
---
+++ design/design-core/components/tag/tag.stylex.ts
@@ -0,0 +1,71 @@
+import stylex from "@stylexjs/stylex";
+import { colors, radius, spacing, sizes } from "../theme/tokens.stylex";
+
+export const styles = stylex.create({
+  tag: {
+    boxSizing: "border-box",
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "12px",
+    height: "24px",
+    lineHeight: "16px",
+    padding: "4px 8px",
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    borderWidth: "1px",
+    borderStyle: "solid",
+  },
+  tag$hidden: {
+    display: "none",
+  },
+  tag$light: (color?: string, bgColor?: string) => ({
+    color: color || colors.primary,
+    backgroundColor: bgColor || colors.border,
+    borderColor: bgColor || colors.border,
+  }),
+  tag$solid: (color?: string) => ({
+    color: "#fff",
+    backgroundColor: color || colors.primary,
+    borderColor: color || colors.primary,
+  }),
+  tag$ghost: (color?: string) => ({
+    backgroundColor: "transparent",
+    color: color || colors.primary,
+    borderColor: color || colors.primary,
+  }),
+  tag$square: {
+    borderRadius: "3px",
+  },
+  tag$circle: {
+    borderRadius: "99px",
+  },
+  prefixIcon: {
+    display: "flex",
+    paddingRight: "4px",
+  },
+  content: {
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+  },
+  suffixIcon: {
+    display: "flex",
+    paddingLeft: "4px",
+  },
+  closeIcon: {
+    paddingLeft: "4px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    color: "#1c1f239e",
+  },
+
+  tagGroup: {
+    height: "26px",
+    display: "flex",
+    alignItems: "center",
+    gap: "8px",
+  },
+});
