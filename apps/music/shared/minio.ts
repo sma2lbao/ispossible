d2044f9e25fbf475295a89870961505fd751a7f4
sma2lbao <sma2lbao@qq.com>
2024-12-09 14:17:59 +0800

    feat: 更新 minio 资源

--- apps/music/shared/minio.ts
+++ apps/music/shared/minio.ts
@@ -2,8 +2,8 @@ import * as Minio from "minio";
 
 export const minio = new Minio.Client({
   endPoint: process.env.MINIO_END_POINT ?? "",
-  port: process.env.MINIO_PORT ? Number(process.env.MINIO_PORT) : 9000,
-  useSSL: false,
+  port: process.env.MINIO_PORT ? Number(process.env.MINIO_PORT) : 9443,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY ?? "",
   secretKey: process.env.MINIO_SECRET_KEY ?? "",
 });
