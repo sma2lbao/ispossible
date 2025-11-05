8affd093f67a4955a829329dd2b1603c1f52cd7d
sma2lbao <sma2lbao@gmail.com>
2025-11-06 00:23:35 +0800

    feat: 删除sw缓存

--- apps/music/providers/swr-provider.tsx
+++ apps/music/providers/swr-provider.tsx
@@ -1,18 +1,12 @@
 "use client";
 
 import { createFetcher } from "@/shared/fetcher";
-import { useEffect } from "react";
 import { SWRConfig } from "swr";
 
 const fetcher = createFetcher();
 
 export const SWRProvider = ({ children }: { children: React.ReactNode }) => {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js").catch(console.error);
-    }
-  }, []);
-
+ 
   return (
     <SWRConfig
       value={{
