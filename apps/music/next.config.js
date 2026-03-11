cc943e15319e09c7c7a043e2d765c76d0817cf5d
sma2lbao <sma2lbao@gmail.com>
2026-03-11 23:39:13 +0800

    feat: 增加sentry监控

--- apps/music/next.config.js
+++ apps/music/next.config.js
@@ -1,10 +1,30 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const stylexPlugin = require("@stylexswc/nextjs-plugin");
+const sentry = require("@sentry/nextjs");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["js", "jsx", "ts", "tsx"],
-};
+const nextConfig = sentry.withSentryConfig(
+  {
+    pageExtensions: ["js", "jsx", "ts", "tsx"],
+  },
+  {
+    org: "sma2lbao-27",
+    project: "music",
+    // Only print logs for uploading source maps in CI
+    // silent: !process.env.CI,
+    // Webpack-specific options for Pages Router
+    webpack: {
+      // Auto-instrument API routes and data fetching methods (default: true)
+      autoInstrumentServerFunctions: true,
+      // Auto-instrument middleware (default: true)
+      autoInstrumentMiddleware: true,
+      // Tree-shake Sentry logger statements to reduce bundle size
+      treeshake: {
+        removeDebugLogging: true,
+      },
+    },
+  },
+);
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 module.exports = stylexPlugin({
